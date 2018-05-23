7d111b38ec2e2bb2f85cc3ec31bddffe76e380f9
Elliot Briant <elliotbriant@Elliots-MacBook-Pro.local>
2018-05-23 13:43:30 -0700

    test library inc and dec function

--- Documents/codes/solidity-examples/test_library.sol
+++ Documents/codes/solidity-examples/test_library.sol
@@ -1,1 +1,23 @@
 pragma solidity ^0.4.0;
+
+import "./libraries.sol";
+
+contract TestLibrary {
+  using IntExtended for uint;
+
+  function testIncrement(uint _base) returns (uint) {
+    return _base.increment();
+  }
+
+  function testDecrement(uint _base) returns (uint) {
+    return _base.decrement();
+  }
+
+  function testIncrementByValue(uint _base, uint _value) returns (uint) {
+    return _base.incrementByValue(_value);
+  }
+
+  function testDecrementByValue(uint _base, uint _value) returns (uint) {
+    return _base.decrementByValue(_value);
+  }
+}
